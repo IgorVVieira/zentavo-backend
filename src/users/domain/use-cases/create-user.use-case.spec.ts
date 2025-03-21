c7ac7be5f4cfe463e16a54cac6b7ff8e28d2570d
IgorVVieira <igor.gutoo63@gmail.com>
2025-03-21 19:53:42 -0300

    test: ensure create user successfully

---
+++ src/users/domain/use-cases/create-user.use-case.spec.ts
@@ -0,0 +1,65 @@
+import { EntityAlreadyExistsError } from "@shared/errors/entity-already-exists.error";
+import { CreateUserUseCase } from "../../domain/use-cases/create-user.use-case";
+
+describe("CreateUserUseCase", () => {
+  const makeSut = () => {
+    const userRepository = {
+      update: jest.fn(),
+      delete: jest.fn(),
+      find: jest.fn(),
+      findAll: jest.fn(),
+      findByEmail: jest.fn(),
+      create: jest.fn(),
+    };
+    const sut = new CreateUserUseCase(userRepository);
+    return { sut, userRepository };
+  };
+
+  it("should create a user", async () => {
+    const { sut, userRepository } = makeSut();
+    userRepository.findByEmail.mockResolvedValue(null);
+    userRepository.create.mockResolvedValue({
+      id: "any_id",
+      email: "any_email",
+      password: "any_password",
+      name: "any_name",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    const user = await sut.execute({
+      email: "any_email",
+      password: "any_password",
+      name: "any_name",
+    });
+    expect(user).toEqual({
+      id: "any_id",
+      email: "any_email",
+      password: "any_password",
+      name: "any_name",
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    });
+  });
+
+  it("should not create a user if it already exists", async () => {
+    const { sut, userRepository } = makeSut();
+    userRepository.findByEmail.mockResolvedValue({
+      id: "any_id",
+      email: "any_email",
+      password: "any_password",
+      name: "any_name",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    try {
+      await sut.execute({
+        email: "any_email",
+        password: "any_password",
+        name: "any_name",
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(EntityAlreadyExistsError);
+      expect(error.message).toBe("User already exists");
+    }
+  });
+});
