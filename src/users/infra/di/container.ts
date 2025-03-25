e58c3e14170e854546dd447d006021858028764a
IgorVVieira <igor.gutoo63@gmail.com>
2025-03-25 19:57:55 -0300

    chore: di container improvements

--- src/users/infra/di/container.ts
+++ src/users/infra/di/container.ts
@@ -1,14 +1,19 @@
 import { container } from 'tsyringe';
 
-import { EncptyAdapter } from 'users/adapters/gateways/encypt.adapter';
-import { JwtAdapter } from 'users/adapters/gateways/jwt.adapter';
-import { IEncryptPort } from 'users/core/gateways/encypt.port';
-import { IJwtPort } from 'users/core/gateways/jwt.port';
-import { CreateUserUseCase } from 'users/core/use-cases/create-user.use-case';
-import { LoginUseCase } from 'users/core/use-cases/login.use-case';
+import { EncptyAdapter } from '@users/adapters/gateways/encypt.adapter';
+import { JwtAdapter } from '@users/adapters/gateways/jwt.adapter';
+import { UserRepositoryAdapter } from '@users/adapters/repositories/user.repository.adapter';
+import { IUserRepositoryPort } from '@users/core/domain/repositories/user.repository.port';
+import { IEncryptPort } from '@users/core/gateways/encypt.port';
+import { IJwtPort } from '@users/core/gateways/jwt.port';
+import { CreateUserUseCase } from '@users/core/use-cases/create-user.use-case';
+import { LoginUseCase } from '@users/core/use-cases/login.use-case';
 
+container.registerSingleton<IUserRepositoryPort>('UserRepository', UserRepositoryAdapter);
 container.registerSingleton<IEncryptPort>('EncrypterAdapter', EncptyAdapter);
 container.registerSingleton<IJwtPort>('JwtAdapter', JwtAdapter);
 
 container.registerSingleton(CreateUserUseCase);
 container.registerSingleton(LoginUseCase);
+
+export { container };
