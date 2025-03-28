c49c7298e465618f5cb8b35c3bfaf8dad9e35c4d
IgorVVieira <igor.gutoo63@gmail.com>
2025-03-28 18:06:40 -0300

    feat: validate if category exists in CreateCategoryUseCase

--- src/transactions/core/use-cases/category/create-category.use-case.ts
+++ src/transactions/core/use-cases/category/create-category.use-case.ts
@@ -1,6 +1,7 @@
 import { injectable } from 'tsyringe';
 
 import { IBaseUseCase } from '@shared/domain/use-cases/base.use-case';
+import { EntityAlreadyExistsError } from '@shared/errors/entity-already-exists.error';
 
 import { CreateCategoryDto } from '@transactions/application/dto/in/create-category.dto';
 import { CategoryDto } from '@transactions/application/dto/out/category.dto';
@@ -11,6 +12,15 @@ export class CreateCategoryUseCase implements IBaseUseCase<CreateCategoryDto, Ca
   public constructor(private readonly categoryRepository: ICategoryRepositoryPort) {}
 
   public async execute(data: CreateCategoryDto): Promise<CategoryDto> {
+    const categoryExists = await this.categoryRepository.findByNameAndUserId(
+      data.name,
+      data.userId,
+    );
+
+    if (categoryExists) {
+      throw new EntityAlreadyExistsError('Category');
+    }
+
     const category = await this.categoryRepository.create(data);
 
     return {
