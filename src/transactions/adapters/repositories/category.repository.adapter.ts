f8f7577abdaacb8de572921caf07b01248dfd40e
IgorVVieira <igor.gutoo63@gmail.com>
2025-03-28 18:07:43 -0300

    feat: add CategoryRepositoryAdapter

---
+++ src/transactions/adapters/repositories/category.repository.adapter.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import { injectable } from 'tsyringe';
+
+import { BaseRepository } from '@shared/repositories/base.repository';
+
+import { CategoryEntity } from '@transactions/core/domain/entities/category';
+import { ICategoryRepositoryPort } from '@transactions/core/domain/repositories/category.repositor.port';
+
+@injectable()
+export class CategoryRepositoryAdapter
+  extends BaseRepository<CategoryEntity>
+  implements ICategoryRepositoryPort
+{
+  private readonly prisma: PrismaClient;
+
+  public constructor() {
+    const prisma = new PrismaClient();
+
+    super(prisma.category);
+    this.prisma = prisma;
+  }
+
+  public async findByNameAndUserId(name: string, userId: string): Promise<CategoryEntity | null> {
+    const category = await this.prisma.category.findFirst({
+      where: {
+        name,
+        userId,
+      },
+    });
+
+    if (!category) {
+      return null;
+    }
+
+    return category;
+  }
+}
