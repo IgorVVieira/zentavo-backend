da6cd4b081a51889607cdf2a57fe186d98517f7e
IgorVVieira <igor.gutoo63@gmail.com>
2025-03-28 18:26:57 -0300

    test: add CategoryRepositoryAdapter

---
+++ src/transactions/core/use-cases/category/create-category.use-case.spec.ts
@@ -0,0 +1,81 @@
+import { baseRepository, BaseRepositoryMock } from '@shared/test/mocks/base-repository';
+
+import { CreateCategoryUseCase } from '@transactions/core/use-cases/category/create-category.use-case';
+
+describe('CreateCategoryUseCase', () => {
+  type SutTypes = {
+    sut: CreateCategoryUseCase;
+    categoryRepositoryMock: BaseRepositoryMock & {
+      findByNameAndUserId: jest.Mock;
+    };
+  };
+
+  const makeSut = (): SutTypes => {
+    const categoryRepositoryMock = {
+      ...baseRepository,
+      findByNameAndUserId: jest.fn(),
+    };
+
+    const sut = new CreateCategoryUseCase(categoryRepositoryMock);
+
+    return { sut, categoryRepositoryMock };
+  };
+
+  it('should create a category', async () => {
+    const { sut, categoryRepositoryMock } = makeSut();
+
+    categoryRepositoryMock.create.mockResolvedValue({
+      id: 'any_id',
+      userId: 'any_user_id',
+      name: 'any_name',
+      color: 'any_color',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const result = await sut.execute({
+      userId: 'any_user_id',
+      name: 'any_name',
+      color: 'any_color',
+    });
+
+    expect(categoryRepositoryMock.create).toHaveBeenCalledWith({
+      userId: 'any_user_id',
+      name: 'any_name',
+      color: 'any_color',
+    });
+
+    expect(result).toEqual({
+      id: 'any_id',
+      userId: 'any_user_id',
+      name: 'any_name',
+      color: 'any_color',
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    });
+  });
+
+  it('should throw if category already exists', async () => {
+    const { sut, categoryRepositoryMock } = makeSut();
+
+    categoryRepositoryMock.findByNameAndUserId.mockResolvedValueOnce({
+      id: 'any_id',
+      userId: 'any_user_id',
+      name: 'any_name',
+      color: 'any_color',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    try {
+      await sut.execute({
+        userId: 'any_user_id',
+        name: 'any_name',
+        color: 'any_color',
+      });
+    } catch (error) {
+      expect(error.message).toEqual('Category already exists');
+      expect(error.name).toEqual('EntityAlreadyExistsError');
+    }
+  });
+});
