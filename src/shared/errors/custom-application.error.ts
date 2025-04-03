afd3e31f45db244fab9f0f62e318f7dda87c068e
IgorVVieira <igor.gutoo63@gmail.com>
2025-04-03 18:52:45 -0300

    refactor: set CustomApplicationError as abstact class

--- src/shared/errors/custom-application.error.ts
+++ src/shared/errors/custom-application.error.ts
@@ -1,6 +1,6 @@
 import { HttpStatus } from '@shared/http-status.enum';
 
-export class CustomApplicationError extends Error {
+export abstract class CustomApplicationError extends Error {
   constructor(
     message: string,
     protected readonly statusCode: HttpStatus,
