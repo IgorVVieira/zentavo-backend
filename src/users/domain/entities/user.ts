b13c6a5469b2c4dc0149dec115bdb39a6e29f4d5
IgorVVieira <igor.gutoo63@gmail.com>
2025-04-16 18:36:14 -0300

    feat: set user status on entity

--- src/users/domain/entities/user.ts
+++ src/users/domain/entities/user.ts
@@ -1,7 +1,14 @@
 import { BaseEntity } from 'shared/domain/entities/base-entity';
 
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  PEDING_VERIFICATION = 'PENDING_VERIFICATION',
+  BLOCKED = 'BLOCKED',
+}
+
 export class User extends BaseEntity {
   name: string;
   email: string;
   password: string;
+  status: UserStatus;
 }
