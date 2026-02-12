9c8a8681bacf98aa7ff1fa98ef9887cc48509e7f
IgorVVieira <igor.gutoo63@gmail.com>
2026-02-12 19:27:01 -0300

    chore: improve api security with helmet, express-rate-limit and cors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import 'module-alias/register';
 import 'reflect-metadata';
 
 import cors from 'cors';
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
 import { pinoHttp } from 'pino-http';
 import swaggerUi from 'swagger-ui-express';
 
@@ -22,15 +24,32 @@ import express from 'express';
 
 import { startConsumers } from './workers';
 
+const limiter = rateLimit({
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
 const app = express();
 
 app.use(pinoHttp());
-app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    origin: process.env.NODE_ENV === 'dev' ? '*' : process.env.FRONTEND_URL,
   }),
 );
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  }),
+);
+app.use(express.json());
+app.use(limiter);
 
 app.use('/api', userRouter, transactionRouter, paymentRouter);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
